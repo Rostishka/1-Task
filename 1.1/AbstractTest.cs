524b8c4a5e19a854afccd05787a5b0bac7bd326f
Diakiv Rostyslav <rostik0945@gmail.com>
2017-03-21 14:43:48 +0200

    Added abstract class and xml staff for console view

---
+++ 1.1/AbstractTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1
+{
+    public abstract class AbstractTest
+    {
+        public List<Department> departments = new List<Department>();
+        public List<Book> booooks = new List<Book>();
+        public int _books;
+        public string _name;
+    }
+}
---
+++ 1.1/ExtObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace _1._1
+{
+    public static class ExtObject
+    {
+        public static string XmlSerialize<T>(this T objectToSerialize)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+
+            StringWriter stringWriter = new StringWriter();
+            XmlTextWriter xmlWriter = new XmlTextWriter(stringWriter);
+
+            xmlWriter.Formatting = Formatting.Indented;
+            xmlSerializer.Serialize(xmlWriter, objectToSerialize);
+
+            return stringWriter.ToString();
+        }
+    }
+}
