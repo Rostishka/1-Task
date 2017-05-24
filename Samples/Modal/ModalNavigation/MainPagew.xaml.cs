e23b133cc0515b5e3066890e1b7b23dd8feb4e64
rostishka <rostik0945@gmail.com>
2017-05-24 23:02:16 +0300

    so

---
+++ Samples/Modal/ModalNavigation/MainPagew.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ModalNavigation
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPagew : ContentPage
+    {
+        List<Contact> contacts;
+
+        public MainPagew()
+        {
+            InitializeComponent();
+
+            SetupData();
+            listView.ItemsSource = contacts;
+        }
+        async void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (listView.SelectedItem != null)
+            {
+                var detailPage = new DetailPage();
+                detailPage.BindingContext = e.SelectedItem as Contact;
+                listView.SelectedItem = null;
+                await Navigation.PushModalAsync(detailPage);
+            }
+        }
+
+        void SetupData()
+        {
+            contacts = new List<Contact>();
+            contacts.Add(new Contact
+            {
+                Name = "Jane Doe",
+                Age = 30,
+                Occupation = "Developer",
+                Country = "USA"
+            });
+            contacts.Add(new Contact
+            {
+                Name = "John Doe",
+                Age = 34,
+                Occupation = "Tester",
+                Country = "USA"
+            });
+            contacts.Add(new Contact
+            {
+                Name = "John Smith",
+                Age = 52,
+                Occupation = "PM",
+                Country = "UK"
+            });
+            contacts.Add(new Contact
+            {
+                Name = "Kath Smith",
+                Age = 55,
+                Occupation = "Business Analyst",
+                Country = "UK"
+            });
+            contacts.Add(new Contact
+            {
+                Name = "Steve Smith",
+                Age = 19,
+                Occupation = "Junior Developer",
+                Country = "UK"
+            });
+        }
+    }
+}
